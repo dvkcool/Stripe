095c46536918e74a686a78b0996204c36bf9465b
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-02-12 19:57:42 +0530

    Adding varried files for modular code

--- native-app/src/Root.js
+++ native-app/src/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Root from './src/Root'
+
 export default class App extends React.Component {
   render() {
     return (
